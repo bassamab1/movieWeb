b44a6c0018a822daccc63b385f69473b129a7264
Bassam Abbas <abbasbassam823@gmail.com>
2026-05-23 16:27:10 +0300

    add rate limit to getmovies

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -6,10 +6,11 @@ import {
 } from "../controllers/moviesControllers.js";
 import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
-router.get("/movies", authMiddleware, getMovies);
+router.get("/movies",rateLimit ,authMiddleware, getMovies);
 router.post("/movies",authMiddleware,adminMiddleware, createMovie);
 router.delete("/movies/:id",authMiddleware,adminMiddleware, deleteMovie);
 export default router;
