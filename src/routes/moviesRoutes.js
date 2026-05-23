91f6897fe011cc81fe14228f8ffdb06941967f24
Bassam Abbas <abbasbassam823@gmail.com>
2026-05-23 16:50:01 +0300

    fix error name for ratelimite

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -6,11 +6,11 @@ import {
 } from "../controllers/moviesControllers.js";
 import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { apiLimiter } from "../middlewares/rateLimiter.js";
+import { apiLimiter } from "../middlewares/rateLimiter.js"
 
 const router = express.Router();
 
-router.get("/movies",rateLimit ,authMiddleware, getMovies);
+router.get("/movies",apiLimiter ,authMiddleware, getMovies);
 router.post("/movies",authMiddleware,adminMiddleware, createMovie);
 router.delete("/movies/:id",authMiddleware,adminMiddleware, deleteMovie);
 export default router;
